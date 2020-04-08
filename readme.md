5032437353879a9eeb75d818437b1db2ecea7753
Axel Charpentier <axelc@ayctor.com>
2020-04-08 19:00:42 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,53 @@
+# SireneAPI
+
+It is a small php client library to use the [Sirene API](https://entreprise.data.gouv.fr/api_doc_sirene).
+
+See [official documentation](https://entreprise.data.gouv.fr/api_doc_sirene) for availables methods.
+
+## Installation
+
+```
+composer require ayctor/sireneapi
+```
+
+## Usage
+
+```php
+use SireneApi\SireneApi;
+
+$api = new SireneApi;
+
+// Get all companies
+$companies = $api->companies()->all();
+// Get company by SIREN number
+$company = $api->companies()->getBySiren('552081317');
+// Get companies by other fields
+$companies = SirenApi::companies()->getBy('code_postal', 77100);
+// Get companies by other fields
+$companies = $api->companies()->getBy([
+    'code_postal' => 77100,
+]);
+
+// Get all estabslishments
+$establishments = $api->establishments()->all();
+// Get establishments by SIREN number
+$establishments = $api->establishments()->getBySiren('552081317');
+// Get establishment by SIRET number
+$establishment = $api->establishments()->getBySiret('55208131785027');
+// Get establishments by other fields
+$establishments = $api->establishments()->getBy('code_postal', 77100);
+// Get establishments by other fields
+$establishments = SirenApi::establishments()->getBy([
+    'code_postal' => 77100,
+]);
+```
+
+## Errors
+
+### getBySiret()
+
+It will throw an exception if you use this function to retrieve companies
+
+### getBy()
+
+It will throw an exception if you use this function with the first param as a string and no second param.
---
+++ src/SireneApi.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace SireneApi;
+
+use GuzzleHttp\Client;
+
+class SireneApi
+{
+    /**
+     * Base api endpoint
+     *
+     * @var  string
+     */
+    private string $endpoint = 'https://entreprise.data.gouv.fr/api/sirene/v3/';
+
+    /**
+     * GuzzleHttp client
+     *
+     * @var  null
+     */
+    private $client = null;
+
+    /**
+     * API method
+     *
+     * @var  string
+     */
+    private string $method = 'unites_legales';
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => $this->endpoint,
+        ]);
+    }
+
+    /**
+     * Set the method to companies
+     *
+     * @return  \SireneApi\SireneApi
+     */
+    public function companies(): self
+    {
+        $this->method = 'unites_legales';
+
+        return $this;
+    }
+
+    /**
+     * Set the method to establishments
+     *
+     * @return  \SireneApi\SireneApi
+     */
+    public function establishments(): self
+    {
+        $this->method = 'etablissements';
+
+        return $this;
+    }
+
+    /**
+     * Get all companies or establishments
+     *
+     * @return  array
+     */
+    public function all(): array
+    {
+        return $this->request($this->method);
+    }
+
+    /**
+     * Get establishment by siret
+     *
+     * @throws  \Exception  if the method is not "etablissements"
+     *
+     * @return  \SirenApi\SireneApi
+     */
+    public function getBySiret(string $siret): array
+    {
+        if ($this->method !== 'etablissements') {
+            throw new \Exception('API method must be set to "etablissements". Use the establisments() method first in order to set the method.');
+        }
+
+        return $this->request($this->method, [
+            'siret' => $siret,
+        ]);
+    }
+
+    /**
+     * Get establishments by siren
+     *
+     * @return  \SirenApi\SireneApi
+     */
+    public function getBySiren(string $siren): array
+    {
+        return $this->request($this->method, [
+            'siren' => $siren,
+        ]);
+    }
+
+    /**
+     * Get establishments by siren
+     *
+     * @param  mixed   $params
+     * @param  string  $value
+     *
+     * @return  \SirenApi\SireneApi
+     */
+    public function getBy($params, ?string $value = null): array
+    {
+        if (is_array($params)) {
+            return $this->request($this->method, $params);
+        } else {
+            if (is_string($params) && !is_null($value)) {
+                return $this->request($this->method, [
+                    $params => $value,
+                ]);
+            } else {
+                throw new \Exception('The first params must be an array or a string. If you set a string, set the value in second params');
+            }
+        }
+    }
+
+    /**
+     * Make an HTTP GET request for retrieving data
+     *
+     * @param  string  $method
+     * @param  array   $params
+     *
+     * @return  array
+     */
+    public function request(string $method, array $params = []): array
+    {
+        $response = $this->client->request('GET', $method, [
+            'query' => $params,
+        ]);
+
+        if ($response->getStatusCode() == 200) {
+            $response = json_decode($response->getBody());
+
+            return $response->$method;
+        }
+
+        return $response;
+    }
+}
